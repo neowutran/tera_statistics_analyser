4e751a0860f43b811d4250bd48c0385e2a901fa5
Neowutran <me@neowutran.ovh>
2018-03-18 15:47:40 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate docopt;
-extern crate lzma;
 extern crate serde_json;
 extern crate rusqlite;
 extern crate glob;
 extern crate r2d2;
+extern crate time;
 extern crate r2d2_sqlite;
 extern crate threadpool;
+extern crate chrono;
 use docopt::Docopt;
 use std::sync::mpsc::{Sender, Receiver};
-use std::fs;
 use std::str;
 use std::fs::File;
 use std::path::Path;
 use std::process::Command;
 use std::io::prelude::*;
-use rusqlite::Connection;
 use r2d2::Pool;
 use r2d2::PooledConnection;
-use std::thread;
 use glob::glob;
 use std::sync::mpsc;
 use r2d2_sqlite::SqliteConnectionManager;
 use threadpool::ThreadPool;
 use threadpool::Builder;
+use std::fs;
+use time::Duration;
+use chrono::prelude::*;
 const USAGE: &'static str = "
 Tera Statistics Analyser.
 
@@ -53,7 +54,7 @@ struct Args {
  * - Export statistics to files
  *
  * TODO a little drawing on how it work
-**/
+ **/
 fn main() {
   let args: Args = Docopt::new(USAGE)
     .and_then(|d| d.deserialize())
@@ -65,7 +66,7 @@ fn main() {
 
   let (tx, rx): (Sender<Vec<StatsLog>>, Receiver<Vec<StatsLog>>) = mpsc::channel();
   let search = format!("{}/**/*.xz", args.flag_source);
-  let thread_pool: ThreadPool  = Builder::new().build();
+  let thread_pool: ThreadPool = Builder::new().build();
   for entry in glob(&search).expect("Failed to read glob pattern") {
     let os_string = entry.unwrap().into_os_string();
     let string = os_string.into_string().unwrap();
@@ -80,21 +81,17 @@ fn main() {
   }
 
   drop(tx);
-  let mut handles = Vec::new();
   for received in rx{
     let db_pool_clone = database_pool.clone();
     // Compute the statistics based on the parsed json
-    let handle = thread::spawn(move || {
+    thread_pool.execute(move || {
       let conn = db_pool_clone.get().unwrap();
       process(conn, received);
       println!("Processing ended");
     });
-    handles.push(handle);
   }
 
-  for handle in handles{
-    handle.join().unwrap();
-  }
+  thread_pool.join();
 
   let db_pool_clone = database_pool.clone();
   //write to file
@@ -105,20 +102,53 @@ fn process(conn: PooledConnection<SqliteConnectionManager>, contents: Vec<StatsL
   class_count::process(conn, contents);
 }
 
-fn export(pool: &Pool<SqliteConnectionManager>, target: String){
-  let thread_pool = pool.clone();
-  let handle = thread::spawn(move || {
+fn export(database_pool: &Pool<SqliteConnectionManager>, target: String){
+  let database_pool_clone = database_pool.clone();
+  let target_copy = target.clone();
+  let thread_pool: ThreadPool = Builder::new().build();
+  thread_pool.execute(move || {
     for region in REGIONS{
-      let conn = thread_pool.get().unwrap();
+      let conn = database_pool_clone.get().unwrap();
       export_region(conn, &target, region);
     }
   });
-  handle.join().unwrap();
+
+  let mut beginning = Utc.datetime_from_str("2018-03-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap();
+  let current:i64 = Utc::now().timestamp();
+
+  let database_pool_clone = database_pool.clone();
+  thread_pool.execute(move || {
+    while beginning.timestamp() < current{
+      let end = beginning + Duration::days(30);
+      for region in REGIONS{
+        let conn = database_pool_clone.get().unwrap();
+        export_region_month(conn, &target_copy, region, beginning, end);
+      }
+      beginning = end;
+    }
+  });
+
+  thread_pool.join();
 }
 
 fn export_region(conn: PooledConnection<SqliteConnectionManager>, target: &String, region: &str){
   let result = class_count::export_region(conn, region);
+  match fs::create_dir(target){
+    Ok(_) => (),
+    Err(e) => println!("{}", e),
+  }
   let filename = format!("{}/{}.txt", target, region);
+  write_file(filename, result);
+}
+
+fn export_region_month(conn: PooledConnection<SqliteConnectionManager>, target: &String, region: &str, date_start: DateTime<Utc>, date_end: DateTime<Utc>){
+  let result = class_count::export_region_month(conn, region, date_start, date_end );
+  let directory = format!("{}/{}/", target, region);
+  let filename = format!("{}/{}-{}.txt", directory, date_start.year(), date_start.month());
+  match fs::create_dir(directory){
+    Ok(_) => (),
+    Err(e) => println!("{}", e),
+  }
   write_file(filename, result);
 }
 
@@ -144,9 +174,9 @@ fn database_initialization() -> Pool<SqliteConnectionManager>{
   // but harder.
   let manager = SqliteConnectionManager::memory();
   let pool = r2d2::Pool::builder()
-        .max_size(1)
-        .build(manager)
-        .unwrap();
+    .max_size(1)
+    .build(manager)
+    .unwrap();
   return pool;
 }
 
@@ -155,6 +185,7 @@ mod class_count{
   use StatsLog;
   use r2d2_sqlite::SqliteConnectionManager;
   use r2d2::PooledConnection;
+  use chrono::prelude::*;
   pub fn initialize(conn: PooledConnection<SqliteConnectionManager>){
     conn.execute("CREATE TABLE player_class (
                   id              INTEGER PRIMARY KEY,
@@ -198,9 +229,23 @@ mod class_count{
     }
     return result;
   }
+
+  pub fn export_region_month(conn: PooledConnection<SqliteConnectionManager>, region: &str, date_start: DateTime<Utc>, date_end: DateTime<Utc>)-> String{
+    let mut result = String::new();
+    {
+      let mut stmt = conn.prepare("SELECT count(1), name from player_class where region = :region and time >= :date_start and time <= :date_end group by name").unwrap();
+      let mut rows = stmt.query_named(&[(":region", &region), (":date_start", &date_start.timestamp()), (":date_end", &date_end.timestamp())]).unwrap();
+      while let Some(result_row) = rows.next() {
+        let row = result_row.unwrap();
+        let count: i64 = row.get(0);
+        let name: String = row.get(1);
+        let line = format!("{}:{}\n", name, count);
+        result.push_str(&line);
+      }
+    }
+    return result;
+  }
 }
-
-
 
 impl StatsLog{
   pub fn new(filename: String) -> Vec<StatsLog>{
@@ -220,8 +265,6 @@ impl StatsLog{
     return json;
   }
 }
-
-
 
 // Full json structure
 
@@ -244,16 +287,16 @@ struct Encounter {
   //debuff_uptime: Vec<Value>,
   //#[serde(rename="encounterUnixEpoch")]
   //encounter_unix_epoch: i64,
-  #[serde(rename="fightDuration")]
-  fight_duration: String,
+  //#[serde(rename="fightDuration")]
+  //fight_duration: String,
   timestamp: i64,
   members: Vec<Members>,
   //#[serde(rename="meterName")]
   //meter_name: String,
   //#[serde(rename="meterVersion")]
   //meter_version: String,
-  #[serde(rename="partyDps")]
-  party_dps: String,
+  //#[serde(rename="partyDps")]
+  //party_dps: String,
   //#[serde(default)]
   //uploader: String, //zero-based index of uploader in members list
 }
@@ -279,18 +322,18 @@ struct Members{
   //player_death_duration: String,
   //#[serde(rename="playerDeaths")]
   //player_deaths: String,
-  #[serde(rename="playerDps")]
-  player_dps: String,
+  //#[serde(rename="playerDps")]
+  //player_dps: String,
   //#[serde(rename="playerId")]
   //player_id:u32,
   //#[serde(rename="playerName")]
   //player_name: String,
-  #[serde(rename="playerServer")]
-  player_server: String,
+  //#[serde(rename="playerServer")]
+  //player_server: String,
   //#[serde(rename="playerTotalDamage")]
   //player_total_damage: String,
-  #[serde(rename="playerTotalDamagePercentage")]
-  player_total_damage_percentage: String,
+  //#[serde(rename="playerTotalDamagePercentage")]
+  //player_total_damage_percentage: String,
   //#[serde(rename="skillLog")]
   //skill_log: Vec<SkillLog>,
   //#[serde(rename="skillCasts")]
