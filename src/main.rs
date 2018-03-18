98435f7396469760c282d2a54b5eb55459b8a4c0
Neowutran <me@neowutran.ovh>
2018-03-18 13:37:19 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ extern crate docopt;
 extern crate lzma;
 extern crate serde_json;
 extern crate rusqlite;
-use serde_json::{Error};
+extern crate glob;
+extern crate r2d2;
+extern crate r2d2_sqlite;
 use docopt::Docopt;
+use std::sync::mpsc::{Sender, Receiver};
 use std::fs;
 use std::str;
 use std::fs::File;
@@ -13,6 +16,13 @@ use std::path::Path;
 use std::process::Command;
 use std::io::prelude::*;
 use rusqlite::Connection;
+use r2d2::Pool;
+use r2d2::PooledConnection;
+use std::thread;
+use glob::glob;
+use std::sync::mpsc;
+use r2d2_sqlite::SqliteConnectionManager;
+
 const USAGE: &'static str = "
 Tera Statistics Analyser.
 
@@ -28,6 +38,7 @@ Options:
   --target <target>   Target data directory [default: ./].
 ";
 
+const REGIONS: &'static [&'static str] = &["EU", "NA", "KR", "JP", "RU", "TW", "KR-PTS", "THA"];
 #[derive(Debug, Deserialize)]
 struct Args {
   flag_source: String,
@@ -38,20 +49,58 @@ fn main() {
   let args: Args = Docopt::new(USAGE)
     .and_then(|d| d.deserialize())
     .unwrap_or_else(|e| e.exit());
-  println!("Source directory is {}", args.flag_source);
-  let mut db = database_initialization();
-  let paths = fs::read_dir(args.flag_source).unwrap();
-  for path in paths {
-    let name = path.unwrap().path();
-    let name = name.to_str().unwrap();
-    if name.ends_with(".xz"){
-      println!("{}", name);
-      let contents = StatsLog::new(&name);
-      db = class_count::process(contents, db);
-    }
+  let pool = database_initialization();
+  let conn = pool.get().unwrap();
+  class_count::initialize(conn);
+
+  let (tx, rx): (Sender<Vec<StatsLog>>, Receiver<Vec<StatsLog>>) = mpsc::channel();
+
+  let search = format!("{}/**/*.xz", args.flag_source);
+  for entry in glob(&search).expect("Failed to read glob pattern") {
+    let os_string = entry.unwrap().into_os_string();
+    let string = os_string.into_string().unwrap();
+    let thread_tx = tx.clone();
+    thread::spawn(move || {
+      let string_copy = string.clone();
+      let contents = StatsLog::new(string);
+      thread_tx.send(contents).unwrap();
+      println!("Parsing finished for {}", string_copy);
+    });
+  }
+  for received in rx{
+    let thread_pool = pool.clone();
+    thread::spawn(move || {
+      let conn = thread_pool.get().unwrap();
+      process(conn, received);
+      println!("Processing ended");
+    });
   }
 
-  class_count::export(db);
+
+  let thread_pool = pool.clone();
+  export(&thread_pool, args.flag_target);
+}
+
+fn process(conn: PooledConnection<SqliteConnectionManager>, contents: Vec<StatsLog>){
+  class_count::process(conn, contents);
+}
+
+fn export(pool: &Pool<SqliteConnectionManager>, target: String){
+  let thread_pool = pool.clone();
+  thread::spawn(move || {
+    for region in REGIONS{
+      let conn = thread_pool.get().unwrap();
+      export_region(conn, &target, region);
+    }
+  });
+}
+
+fn export_region(conn: PooledConnection<SqliteConnectionManager>, target: &String, region: &str){
+  let result = class_count::export_region(conn, region);
+  let filename = format!("{}/{}.txt", target, region);
+  println!("write {}", filename);
+  write_file(filename, result);
+  // return (db, target);
 }
 
 fn write_file(name: String, content: String){
@@ -70,17 +119,20 @@ fn write_file(name: String, content: String){
   }
 }
 
-fn database_initialization() -> Connection{
-  let mut conn = Connection::open_in_memory().unwrap();
-  conn = class_count::initialize(conn);
-  return conn;
+fn database_initialization() -> Pool<SqliteConnectionManager>{
+//  let manager = SqliteConnectionManager::memory();
+  let manager = SqliteConnectionManager::file("stats.db?mode=memory&cache=shared");
+  let pool = r2d2::Pool::new(manager).unwrap();
+  return pool;
 }
+
 
 mod class_count{
   use StatsLog;
-  use rusqlite::Connection;
-  pub fn initialize(db: Connection) -> Connection{
-    db.execute("CREATE TABLE player_class (
+  use r2d2_sqlite::SqliteConnectionManager;
+  use r2d2::PooledConnection;
+  pub fn initialize(conn: PooledConnection<SqliteConnectionManager>){
+    conn.execute("CREATE TABLE player_class (
                   id              INTEGER PRIMARY KEY,
                   name            TEXT NOT NULL,
                   region          TEXT NOT NULL,
@@ -88,10 +140,10 @@ mod class_count{
                   boss_id         INTEGER NOT NULL,
                   time            INTEGER NOT NULL
                   )", &[]).unwrap();
-    return db
+    println!("player_class table created");
   }
 
-  pub fn process(contents: Vec<StatsLog>, db: Connection) -> Connection {
+  pub fn process(conn: PooledConnection<SqliteConnectionManager>, contents: Vec<StatsLog>) {
     for content in contents{
       let directory_split = content.directory.split(".");
       let directory_vec: Vec<&str> = directory_split.collect();
@@ -101,38 +153,37 @@ mod class_count{
       let boss:i32 = content.content.boss_id.parse().unwrap();
       for member in content.content.members{
         let class = member.player_class;
-        db.execute_named("INSERT INTO player_class (name, region, dungeon_id, boss_id, time)
+        conn.execute_named("INSERT INTO player_class (name, region, dungeon_id, boss_id, time)
                   VALUES (:name, :region, :dungeon_id, :boss_id, :time)", &[(":name", &class), (":region",&region), (":dungeon_id",&dungeon), (":boss_id",&boss), (":time",&timestamp)]).unwrap();
       }
     }
-    return db;
   }
-  pub fn export(db: Connection) -> Connection{
+  pub fn export_region(conn: PooledConnection<SqliteConnectionManager>, region: &str) -> String{
+    let mut result = String::new();
     {
-      let mut stmt = db.prepare("SELECT count(1), name, region from player_class group by name, region").unwrap();
-      let mut rows = stmt.query(&[]).unwrap();
-      let mut global = String::new();
+      let mut stmt = conn.prepare("SELECT count(1), name from player_class where region = :region group by name").unwrap();
+      let mut rows = stmt.query_named(&[(":region", &region)]).unwrap();
       while let Some(result_row) = rows.next() {
         let row = result_row.unwrap();
         let count: i64 = row.get(0);
         let name: String = row.get(1);
-        let region: String = row.get(2);
-        println!("{} {}: {}", region, name, count);
+        let line = format!("{}:{}\n", name, count);
+        result.push_str(&line);
       }
 
     }
-    return db;
+    return result;
   }
 }
 
 
 
 impl StatsLog{
-  pub fn new(filename: &str) -> Vec<StatsLog>{
-    println!("read {}", &filename);
+  pub fn new(filename: String) -> Vec<StatsLog>{
+    println!("read {}", filename);
     // Rust-lzma crash on magic byte detection for XZ. So back to system version until I found why
     let mut command = String::from("unxz --stdout ");
-    command.push_str(filename);
+    command.push_str(&filename);
     println!("command: {}", command);
     let output = Command::new("sh")
       .arg("-c")
